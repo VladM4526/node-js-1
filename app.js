4191b0123b111bfe79735847f16fc64b4608744e
VladM <vdstories20@gmail.com>
2023-11-01 20:19:13 +0100

    making task

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 const contacts = require("./contacts/contacts");
-
 console.log(contacts);
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, contactId, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactDate = await contacts.listContacts();
+      break;
+    case "get":
+      const contactIdCode = await contacts.getContactById(contactId);
+      return console.log(contactIdCode);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(contactId);
+      return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+const options = program.opts();
+invokeAction(options);
--- contacts/contacts.js
+++ contacts/contacts.js
@@ -14,33 +14,35 @@ async function contactDate() {
 async function listContacts() {
   // ...твій код. Повертає масив контактів.
   const contacts = await contactDate();
-  const result = contacts.find(
-    (item) => item.id,
-    item.name,
-    item.email,
-    item.phone
-  );
-  return result;
+  const result = contacts.map((item) => {
+    return {
+      id: item.id,
+      name: item.name,
+      email: item.email,
+      phone: item.phone,
+    };
+  });
+
+  return console.table(result);
 }
 
 async function getContactById(contactId) {
   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await contactDate();
-  const result = contacts.find((item) => item.contactId === contactId);
-  return result || null;
+  const result = contacts.find((item) => item.id === contactId);
+  return console.log(result || null);
 }
 
 async function removeContact(contactId) {
   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await contactDate();
-  const index = contacts.findIndex((item) => item.contactId === contactId);
+  const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) {
     return null;
   }
-
   const [result] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
+  await fs.writeFile(contactsPath, JSON.stringify(index, null, 2));
+  return console.log(result);
 }
 
 async function addContact(name, email, phone) {
@@ -48,12 +50,10 @@ async function addContact(name, email, phone) {
   const contacts = await contactDate();
   const newContact = {
     name,
-    email,
-    phone,
   };
-  books.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  return newContact;
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return console.log(newContact);
 }
 
 module.exports = {
