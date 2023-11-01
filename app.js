0f31954b550c91482a96955a72ad94267416ebd0
VladM <vdstories20@gmail.com>
2023-11-01 16:09:20 +0100

    make task

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const contacts = require("./contacts");
+const contacts = require("./contacts/contacts");
 
 console.log(contacts);
--- contacts/contacts.js
+++ contacts/contacts.js
@@ -2,28 +2,63 @@
 
 const fs = require("fs/promises");
 
-const contactsPath = require("path");
-const contacts = path.dirname("./db/contacts.json");
+const path = require("path");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function contactDate() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
 
 // TODO: задокументувати кожну функцію
-function listContacts() {
+async function listContacts() {
   // ...твій код. Повертає масив контактів.
+  const contacts = await contactDate();
+  const result = contacts.find(
+    (item) => item.id,
+    item.name,
+    item.email,
+    item.phone
+  );
+  return result;
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await contactDate();
+  const result = contacts.find((item) => item.contactId === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await contactDate();
+  const index = contacts.findIndex((item) => item.contactId === contactId);
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+  const contacts = await contactDate();
+  const newContact = {
+    name,
+    email,
+    phone,
+  };
+  books.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
 }
 
 module.exports = {
-  //   listContacts,
-  contacts,
-  contactsPath,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
 };
-
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
-
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
-
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
